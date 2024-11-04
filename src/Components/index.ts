ac56a4b47b8995309ead277dc22ff2ce85e78c78
Richard LaConte <richl.laconte@gmail.com>
2024-11-04 15:20:00 -0500

    add carousel export

--- src/Components/index.ts
+++ src/Components/index.ts
@@ -6,6 +6,7 @@ export { default as Avatar} from "./Avatar";
 export { default as Badge } from "./Badge";
 export { default as Button } from "./Button";
 export { default as Card } from "./Card";
+export { default as Carousel } from "./Carousel";
 export { default as CheckBox } from "./CheckBox";
 export { default as Chip } from "./Chip";
 export { default as Code } from "./Code";
